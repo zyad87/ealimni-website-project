df9e3022dd6cc7bf6f071f2e9e47ef3bcfa5fcfb
zyad87 <zyadt871@gmail.com>
2024-07-04 10:37:10 +0300

    الصفحة

--- src/index.js
+++ src/index.js
@@ -5,3 +5,31 @@ import "./assets/css/style.css";
 import './assets/js/scripts.js';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
+// src/scripts/main.js
+document.getElementById("contactalirt").style.display = "none";
+
+// التأكد من تحميل DOM بالكامل
+document.addEventListener('DOMContentLoaded', function() {
+    // الحصول على النموذج
+    const contactForm = document.getElementById('contactForm');
+    
+    // الاستماع لحدث الإرسال
+    contactForm.addEventListener('submit', function(event) {
+      // إيقاف الإرسال الافتراضي
+      event.preventDefault();
+      
+      // التحقق من صحة البيانات
+      if (!contactForm.checkValidity()) {
+        event.stopPropagation();
+      } else {
+        // يمكنك إضافة الشيفرة هنا لإرسال البيانات إلى الخادم
+        document.getElementById("contactform").style.display = "none";
+        document.getElementById("contactalirt").style.display = "block";
+
+      }
+      
+      // إضافة فئة التحقق
+      contactForm.classList.add('was-validated');
+    }, false);
+  });
+  document.getElementById('fullYear').innerHTML = new Date().getFullYear();
